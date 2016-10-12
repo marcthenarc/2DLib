2fa3507c92cf9025d550a328adfc4b945e7cd824
Marc St-Jacques <marc@geekchef.com>
2016-10-11 21:55:56 -0400

    Additionnal methods for class Rect.

--- 2DLib/rect.h
+++ 2DLib/rect.h
@@ -25,4 +25,28 @@ public:
 
 	Rect() { }
 	Rect(const Point& p, const Size& s) : P(p), S(s) { }
+	Rect(const Point &p, const Point &q) : P(p), S((q - p).X, (q - p).Y) { }
+
+	void Grow(int g)
+	{
+		P.X -= g;
+		P.Y -= g;
+
+		S.W += g * 2;
+		S.H += g * 2;
+	}
+
+	void Shrink(int s)
+	{
+		P.X += s;
+		P.Y += s;
+
+		S.W += s * 2;
+		S.H -= s * 2;
+	}
+
+	Point GetBottomRight() const
+	{
+		return Point(P.X + S.W, P.Y + S.H);
+	}
 };
